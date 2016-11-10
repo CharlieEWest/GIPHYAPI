ba876f67fcd53db122fe6a04f98bb4413238e9a4
charles West <charleswest33@gmail.com>
2016-11-09 20:35:23 -0600

    API git save

---
+++ main.js
@@ -0,0 +1,7 @@
+var ourRequest = new XMLHttpRequest();
+	ourRequest.open('GET','' + pageCounter + '.json');
+	ourRequest.onload = function() {
+		console.log(ourRequest.responseText);
+		var ourData = 
+	};
+	ourRequest.send();
